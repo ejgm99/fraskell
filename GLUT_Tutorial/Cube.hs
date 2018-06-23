ac837cc206cd7848f5f8da5d5ccf05593d7a0a74
ejgm <ejgomezmarino@gmail.com>
2018-06-23 13:18:21 -0600

    got empty squares ready

--- GLUT_Tutorial/Cube.hs
+++ GLUT_Tutorial/Cube.hs
@@ -8,5 +8,9 @@ vertex3f (x, y, z) = vertex $ Vertex3 x y z
 cube :: GLfloat -> IO ()
 cube w = renderPrimitive Quads $ mapM_ vertex3f [( w, w, w), ( w, w,-w), ( w,-w,-w), ( w,-w, w), ( w, w, w), ( w, w,-w), (-w, w,-w), (-w, w, w), ( w, w, w), ( w,-w, w), (-w,-w, w), (-w, w, w), (-w, w, w), (-w, w,-w), (-w,-w,-w), (-w,-w, w), ( w,-w, w), ( w,-w,-w), (-w,-w,-w), (-w,-w, w), ( w, w,-w), ( w,-w,-w), (-w,-w,-w), (-w, w,-w)]
 
-quad :: GLfloat -> IO ()
-quad w = renderPrimitive Points $ mapM_ vertex3f [( w, w, w), ( w, w,-w), ( w,-w,-w), ( w,-w, w), ( w, w, w), ( w, w,-w), (-w, w,-w), (-w, w, w), ( w, w, w), ( w,-w, w), (-w,-w, w), (-w, w, w), (-w, w, w), (-w, w,-w), (-w,-w,-w), (-w,-w, w), ( w,-w, w), ( w,-w,-w), (-w,-w,-w), (-w,-w, w), ( w, w,-w), ( w,-w,-w), (-w,-w,-w), (-w, w,-w)]
+vertex2f :: (GLfloat, GLfloat) -> IO ()
+vertex2f (x, y) = vertex $ Vertex3 x y 0
+
+square :: GLfloat -> IO ()
+square w = renderPrimitive LineLoop $ mapM_ vertex2f [( w, w), ( -w, w), ( -w, -w), ( w,-w)]
+
--- GLUT_Tutorial/Display.hs
+++ GLUT_Tutorial/Display.hs
@@ -9,9 +9,9 @@ display :: DisplayCallback
 display = do
   clear [ColorBuffer]
   scale 0.7 0.7 (0.7::GLfloat)
-  forM_ (points 7) $ \(x,y,z) ->
+  forM_ (points 12) $ \(x,y,z) ->
     preservingMatrix $ do
       color $ Color3 ((x+1)/2) ((y+1)/2) ((z+1)/2)
       translate $ Vector3 x y z
-      quad 0.1
+      square 0.1
   flush
